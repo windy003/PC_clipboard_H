a6f5da51edd8b9d5f035e084e5539931ea240978
windy003 <928920471@qq.com>
2025-05-31 19:22:09 +0800

    增加了头部注释信息

--- 1.py
+++ 1.py
@@ -2,8 +2,8 @@
 # pyinstaller   打包命令:
 # pyinstaller --noconfirm --onefile --windowed --icon=icon.ico --add-data "icon.ico;."   --add-data "icon.png;." 1.py   --name  "clipboard_H"
 
-
-
+# 重要文件路径:
+# "C:\Users\windy\.clipboard_favorites.json"
 
 
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QListWidget, 
